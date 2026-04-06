809a1e022e8023af3c8f8f1ab534fc7745cbe4dc
Claudio Neves <cfneguacu@gmail.com>
2026-04-05 22:13:46 -0300

    Atualizalção do codigo

--- src/main/java/Pi.java
+++ src/main/java/Pi.java
@@ -5,6 +5,7 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
+
 //Desafio pi, achar o primo palindromo de 9 digitos da extensao decimal de pi 
 
 public class Pi {
@@ -15,13 +16,12 @@ public class Pi {
         String numero;
         URL url = new URL("https://www.angio.net/pi/digits/pi1000000.txt");
         BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
-        List<String> palindromos = new ArrayList<>();
+        //List<Integer> palindromos = new ArrayList<>();
         String pi = reader.readLine();
-        int y;
-        int primo;
+
         for (int i = 0; i < pi.length(); i++) {
-            if (i  < pi.length() - 9) {
-               numero = pi.substring(i, i + 9);
+            if (i < pi.length() - 9) {
+                numero = pi.substring(i, i + 9);
                 for (int j = 1; j <= numero.length() / 2; j++) {
                     if (numero.charAt(j - 1) == numero.charAt(numero.length() - j)) {
                         eigual = true;
@@ -31,24 +31,39 @@ public class Pi {
                     }
                 }
                 if (eigual) {
-                    palindromos.add(numero);
+                    int num = Integer.parseInt(numero) % 2 == 1 ? Integer.parseInt(numero) : 0;
+                    if(ehPrimo(num) && num > 0){
+                        System.out.println(num);
+                        break;
+                    }
                 }
             }
         }
-        for (String s : palindromos) {
-            y = 1;
-            primo = 0;
-            while (y <= Integer.parseInt(s)) {
-                if (Integer.parseInt(s) % y == 0) {
-                    primo += 1;
+    }
+
+        public static boolean ehPrimo(Integer numero) {
+
+            /*return (numero <= 2 || numero % 2 != 0)
+                    && (numero <= 3 || numero % 3 != 0)
+                    && (numero <= 5 || numero % 5 != 0)
+                    && (numero <= 7 || numero % 7 != 0)
+                    && (numero <= 11 || numero % 11 != 0)
+                    && (Math.sqrt(numero) % (int) Math.sqrt(numero) != 0);
+                    */
+            int cont = 2;
+
+
+            while (cont < Math.sqrt(numero)) {
+                if (numero % cont == 0) {
+                     return false;
+
                 }
-                y++;
+
+                cont++;
             }
-            if (primo == 2) {
-                System.out.print(s);
-                break;
-            }
-        }
+            return true;
+
+
     }
 }
 
